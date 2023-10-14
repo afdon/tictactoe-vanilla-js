b6df4fa0d49b8c3c1f232bfc9d3d07c6557c5cc3
afdon <53676753+afdon@users.noreply.github.com>
2023-10-14 09:18:09 -0700

    cleanup

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let moves = []
+
+if (moves.length % 2 === 0) {
+    playerEven.isTurn = true;
+} else {
+
+}
+
+let playerOdd = {
+    name: "odd",
+    isTurn: false,
+}
+
+let playerEven = {
+    name: "even",
+    isTurn: false,
+}
